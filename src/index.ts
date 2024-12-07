54f1c4b2b64c59271b2ce56c7a8710da32adcdbf
aryandh.a.w <aryandh.a.w@gmail.com>
2024-12-07 21:56:17 +0700

    integrasi service

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-console.log("Hello world");
+import { CartService } from "./services/cartSerivice";
+import { productService } from "./services/productService";
+
+function runApp() {
+    const cartService = new CartService();
+    window.alert(productService.getProduct());
+
+    while (true) {
+        const input = window.prompt(
+            "Enter product code and quantity (separated by space) or type 'pay' to checkout"
+        )
+
+        if (!input) {
+            break;
+        }
+
+        if (input.toLocaleLowerCase() === 'pay') {
+            cartService.pay();
+            break;
+        }
+
+        const [code, quantityStr] = input.split(" ");
+        const quantityNumber = Number(quantityStr);
+
+        if(isNaN(quantityNumber) || quantityNumber < 0 ) {
+            window.alert("Invalid quantity");
+            continue;
+        }
+
+        cartService.addToCart(code, quantityNumber);
+        cartService.showCart();
+    }
+    window.alert("Thankyour for shopping");
+}
+
+document.getElementById("run")?.addEventListener("click", runApp);
